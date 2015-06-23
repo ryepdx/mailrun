541747150e9cf7124332ab11cc379ad2ccd9c4bc
Ryan Casey <ryepdx@gmail.com>
2015-06-23 13:18:11 -0700

    Add shipping type based on shipping priority in addition to requested shipping type.

--- report/picking_list.py
+++ report/picking_list.py
@@ -28,16 +28,17 @@ class picking_list(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(picking_list, self).__init__(cr, uid, name, context=context)
         company = self.pool.get("res.users").browse(cr, uid, uid, context=context).company_id
+        
         self.localcontext.update({
             'time': time,
             'company': company,
             'mailrunMoves': lambda moves: [m for m in moves if m.mailrun_required],
-            'localMoves': lambda moves: [m for m in moves if not m.mailrun_required]
+            'localMoves': lambda moves: [m for m in moves if not m.mailrun_required],
+            'ship_type': self._get_ship_type,
         })
 
         picking_pool = self.pool.get('stock.picking.out')
         sale_pool = self.pool.get('sale.order')
-
         # Update picking list hashes as necessary.
         for picking in picking_pool.browse(cr, uid, context['active_ids']):
             picking_list_hash = sale_pool.generate_picking_list_hash(picking.sale_id)
@@ -54,7 +55,26 @@ class picking_list(report_sxw.rml_parse):
         picking_pool.write(cr, uid, context['active_ids'], {
             "picking_printed": datetime.datetime.now()
         })
-
+ 
+    def _get_ship_type(self, number):
+        ship_priority = str(number)
+        if not ship_priority:
+            ship_type = ''
+        elif ship_priority[0] == '6':
+            ship_type = 'Ground'
+        elif ship_priority[0] == '3':
+            ship_type = 'VIP'
+        elif ship_priority[0] == '1':
+            ship_type = 'RUSH'
+        elif ship_priority[0] == '2':
+            ship_type = 'Amazon' 
+        elif ship_priority[0] == '4':
+            ship_type = 'Canada'
+        elif ship_priority[0] == '5':
+            ship_type = 'International'
+        else:
+            ship_type = ''
+        return ship_type     
 report_sxw.report_sxw(
     'report.mailrun.picking.list','stock.picking','addons/mailrun/report/picking_list.rml', parser=picking_list
 )
